1b10441a85f85ee744e856a71831f01422a142c7
Lorellana <lorellana.ordonez@gmail.com>
2021-12-14 17:01:43 +0100

    paint balls and paint selected balls

--- js/main.js
+++ js/main.js
@@ -4,19 +4,16 @@
 
 // Un array de números para **Bolitas**: 
 const bingoNumbers = [];
-
-
-
-
+//Un array de números para **Mi cartón**:
+let cardNumbers = [];
 //Un boolean para saber si estamos en modo manual o modo automático, es decir, si han pulsado el **Play** o no: 
 let isPlaying = false;
 
 
-
+//Botones
 const newNumberBtn = document.querySelector('.js-new-number-btn');
 const playBtn = document.querySelector('.js-play-btn');
-//Un array de números para **Mi cartón**:
-let cardNumbers = [];
+
 
 // start page functions
 const init = () => {
@@ -26,9 +23,31 @@ const init = () => {
   paintCardNumbers();
 };
 
+//Función que me genera 1 número aleatorio hasta 100 como máximo.
+function generateRandomNumber() {
+  return Math.ceil(Math.random() * 100);//Este "math.ceil" redondea los números
+}
+
+//Función que genera los 20 números que deben aparecer en el cartón:
+function generateRandomNumbers(){
+  for(let i = 0; i < 20; i++){
+    const randomNumber = generateRandomNumber(100)
+    cardNumbers.push(randomNumber);//Añade los numeros al array, cada numero aleatorio que genera la función "generate randomnumber"
+    //para elminar el numero aleatorio repetido de mi array de card numbers
+      if(randomNumber[i] === randomNumber[i]){
+        cardNumbers.pop;
+      } else{
+        cardNumbers.push(randomNumber);
+      }
+  
+  }
+  console.log(cardNumbers);
+}
+
 // dom listeners
 const handleNewNumber = () => {
   console.log('Se ha pulsado en: Saca una bolita');
+  paintBingoNumbers();
   playRound();
 };
 
@@ -37,35 +56,6 @@ const handlePlay = () => {
   setPlay();
   playRound();
 };
-
-// functions
-
-
-//Genera los 20 números que deben aaprecer en el cartón:
-function generateRandomNumbers(){
-  for(let i = 0; i < 20; i++){
-    const randomNumber = generateRandomNumber(100)
-    cardNumbers.push(randomNumber);//Añade los numeros al array, cada numero aleatorio que genera la función "generate randomnumber"
-    removeRepeatedNumber(cardNumbers);
-  }
-  
-  
-  console.log(cardNumbers);
-}
-
-//Función que me genera 1 número aleatorio hasta 100 como máximo.
-function generateRandomNumber() {
-  return Math.ceil(Math.random() * 100);//Este "math.ceil" redondea los números
-}
-
-//para elminar el numero aleatorio repetido de mi array ed card numbers
-function removeRepeatedNumber(cardNumbers, item){
-let i = cardNumbers.indexOf(item);
-if( i !== -1){
-  cardNumbers.splice(i, 1);
-}
-}
-
 
 
 
@@ -84,21 +74,30 @@ const playRound = () => {
 };
 
 const createNewBingoNumber = () => {
-  console.log('Generar un nuevo número (no repetido) para Bolitas');
+  const randomNumber = generateRandomNumber(100)
+  bingoNumbers.push(randomNumber);
+  paintBingoNumbers();
+  paintCardNumbers();
+
 };
 
+//función para pintar
 const paintBingoNumbers = () => {
-  console.log('Repintar Bolitas');
+  let htmlCode = "";
+  for ( const bingoNumber of bingoNumbers){
+    htmlCode += `<li class="number">${bingoNumber}</li>`;
+  }
+  document.querySelector ('.js-bingo-numbers').innerHTML = htmlCode;
 };
 
 const paintCardNumbers = () => {
-
-  console.log('Repintar todos los números de Mi cartón');
-  console.log('Repintar cada número de Mi cartón');
-
-  let htmlCode = "";
+  let htmlCode = "";//Variable temporal que esta vacia al principio
   for ( const cardNumber of cardNumbers){
-    htmlCode += `<li class="number">${cardNumber}</li>`;
+    if(bingoNumbers.includes(cardNumber)){
+      htmlCode += `<li class="selected">${cardNumber}</li>`;
+    } else{
+      htmlCode += `<li class="number">${cardNumber}</li>`;
+    }
   }
   document.querySelector ('.js-card-numbers').innerHTML = htmlCode;
 
